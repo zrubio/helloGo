af436f2981ed3bfd4518965cb0083d6edabe1050
zrubio <zrubio@protonmail.com>
2025-02-05 12:03:24 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,10 @@
+/* Definimos el paquete */
+package main
+
+/* Importamos las librerías necesarias */
+import "fmt"
+
+/* Definimos la función principal */
+func main() {
+	fmt.Println("Hola Mundo!");
+}
